262a3450a87ec3041ece31a42afc7cb35f78395b
mr-mar493 <85613736+mr-mar493@users.noreply.github.com>
2022-09-05 17:51:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+from telethon import TelegramClient, events, sync
+from os import system
+from sys import platform
+import random
+
+def clear():
+	if platform == "linux" or platform == "linux2" or platform == "unix":
+		system("clear")
+	elif platform == "win32":
+		system("cls")
+	else:
+		system("clear")
+key = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+a = '@'
+for n in range(1):
+	b=''
+	for i in range(5):
+		b += (random.choice(key))
+		c=a+b
+def banner():
+    print("""
+╔════╗╔═══╗     ╔═══╗╔╗──╔╗╔═══╗     ╔═══╗╔═══╗──────╔═╗─╔╗╔╗───╔═══╗╔═══╗╔═══╗
+║╔╗╔╗║║╔═╗║     ║╔═╗║║╚╗╔╝║║╔═╗║     ╚╗╔╗║║╔═╗║──────║║╚╗║║║║───║╔═╗║║╔═╗║╚╗╔╗║
+╚╝║║╚╝║║─╚╝     ║║─║║╚╗║║╔╝║║─║║     ─║║║║║║─║║╔╗╔╗╔╗║╔╗╚╝║║║───║║─║║║║─║║─║║║║
+──║║──║║╔═╗     ║╚═╝║─║╚╝║─║╚═╝║     ─║║║║║║─║║║╚╝╚╝║║║╚╗║║║║─╔╗║║─║║║╚═╝║─║║║║
+──║║──║╚╩═║     ║╔═╗║─╚╗╔╝─║╔═╗║     ╔╝╚╝║║╚═╝║╚╗╔╗╔╝║║─║║║║╚═╝║║╚═╝║║╔═╗║╔╝╚╝║
+──╚╝──╚═══╝     ╚╝─╚╝──╚╝──╚╝─╚╝     ╚═══╝╚═══╝─╚╝╚╝─╚╝─╚═╝╚═══╝╚═══╝╚╝─╚╝╚═══╝
+
+====================================
+YouTube Channel: youtube.com/c/HZFYT
+Telegram Channel: t.me/hzfnews
+VK: vk.com/hzforum1
+====================================
+""")
+
+clear()
+banner()
+id1 = input("Ваш api_id: ")
+hash1 = input("Ваш api_hash1: ")
+api_id = id1
+api_hash = hash1
+client = TelegramClient('an', api_id, api_hash)
+client.start()
+try:
+    client.download_profile_photo(c)
+    client.send_message('@get_any_telegram_id_bot', c)
+except:
+    print('500')
---
+++ s.py
@@ -0,0 +1,5 @@
+from os import system
+import time
+while True:
+    system('python3 main.py')
+    time.sleep(5)
